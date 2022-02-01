82179dfa4c3be0a6ecf8a74e1981a5f5535f3511
Jose Uscanga <jousmo@outlook.com>
2022-02-01 10:03:58 -0600

    feat: initial configuration

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import JournalApp from './JournalApp'
+import './styles/styles.sass'
+
+const divRoot = document.querySelector('#root')
+
+ReactDOM.render(<JournalApp />, divRoot)
