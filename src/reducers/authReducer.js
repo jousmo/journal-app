3dc014de9c1f32ebb08f384564d777e99cf72f9b
José Uscanga <jousmo@outlook.com>
2022-03-26 20:48:29 -0600

    feat: configure base react redux

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,17 @@
+import { types } from '../types'
+
+export const authReducer = (state = {}, action) => {
+  const { type, payload } = !!action && action
+
+  switch (type) {
+    case types.login:
+      return {
+        uid: payload.uid,
+        name: payload.displayName
+      }
+    case type.logout:
+      return {}
+    default:
+      return state
+  }
+}
