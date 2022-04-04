bda470f77ac4fcf2cf21d2346c5f33fdd55fa03b
José Uscanga <jousmo@outlook.com>
2022-04-03 22:09:17 -0500

    feat: implement dayjs in note

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import dayjs from 'dayjs'
 
 export const JournalEntry = ({ note }) => {
-  const { title, body, url } = !!note && note
+  const { title, body, url, date } = !!note && note
+  const strDate = dayjs(date).format('DD/MM/YYYY')
 
   return (
     <li className='journal__entry'>
@@ -19,6 +21,9 @@ export const JournalEntry = ({ note }) => {
       <div className='journal__entry__body'>
         <h3>{title}</h3>
         <p>{body}</p>
+        <p className='text-end fw-lighter m-0'>
+          {strDate}
+        </p>
       </div>
     </li>
   )
